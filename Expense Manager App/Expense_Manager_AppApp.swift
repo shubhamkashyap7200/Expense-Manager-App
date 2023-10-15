8b44c915fa978d76f469f2274dfebb01450a9ec5
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-15 07:07:49 +0530

    Initial Commit

---
+++ Expense Manager App/Expense_Manager_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Manager_AppApp.swift
+//  Expense Manager App
+//
+//  Created by Shubham on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Manager_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
